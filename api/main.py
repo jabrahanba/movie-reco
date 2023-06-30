dbf52784fd62b524d08f3a371ef4f41ef20c12c6
jabrahanba <jabrahanba@gmail.com>
2023-06-30 10:30:47 -0500

    rollback test

--- api/main.py
+++ api/main.py
@@ -1,28 +1,10 @@
 import APIfunctions as f
 from fastapi import FastAPI, HTTPException
-import pandas as pd
 
 #Definir la aplicación
 app = FastAPI()
 
 #http://127.0.0.1:8000 ruta raiz local
-
-'''#Pruebas SI QUEDA TIEMPO PROBAR ESTO SINO BORRAR:
-
-debo agregar:
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
-
-@app.exception_handler(HTTPException)
-async def http_exception_handler(request: Request, exc: HTTPException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"mensaje": "No se puede acceder a la API en este momento o k ase."}
-    )
-
-@app.get("/mi-api")
-def mi_api():
-    raise HTTPException(status_code=500)'''
 
 
 # --------------------------------------------
@@ -95,14 +77,6 @@ async def consulta6(director: str):
         raise HTTPException(status_code=400, detail=str(e))
     
 # --------------------------------------------
-
-#Data lo coloco acá para ver si mejora el rendimiento.
-    pelis = pd.DataFrame
-    pelis = pd.read_csv('../model/df_resize.csv', index_col=False) 
-    df = pd.read_csv('../model/df_model.csv', index_col=False) 
-    trained_model_filename = '../model/trained_model.joblib' 
-    vectorizer_filename = '../model/vectorizer.joblib'
-    column = column = 'stemming_unique'
 
 @app.get("/recomendacion/{title}")
 async def consulta7(title: str):
